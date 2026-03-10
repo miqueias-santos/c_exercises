e403e4608ed468bd35b27a1d44687f6c932c71ec
miqueias-santos <m.qussantos@gmail.com>
2026-03-10 11:34:19 -0300

    questão 3 finalizada

--- list01/l1_ex03.c
+++ list01/l1_ex03.c
@@ -16,29 +16,25 @@ int main(void){
     scanf("%d %d %d", &x, &y, &quad_evitado);
 
     //detectar quadrante
-    if(x > 0 && x > 0) {
+    if(x > 0 && y > 0) {
         Dx = -1 - x;
         Dy = -1 - y;
         quad_destino = 3;
-        printf("Estou no 1, vou pro 3");
     }
     else if(x < 0 && y > 0) {
         Dx = 1 - x;
         Dy = -1 - y;
         quad_destino = 4;
-        printf("Estou no 2, vou pro 4");
     }
     else if(x < 0 && y < 0) {
         Dx = 1 - x;
         Dy = 1 - y;
         quad_destino = 1;
-        printf("Estou no 3, vou pro 1");
     }
     else if(x > 0 && y < 0) {
         Dx = -1 - x;
         Dy = 1 - y;
         quad_destino = 2;
-        printf("Estou no 4, vou pro 2");
     }
 
     if(quad_evitado == quad_destino || abs(quad_evitado - quad_destino) == 2) printf("caminhada invalida\n");
@@ -46,19 +42,19 @@ int main(void){
         //tome x
         x += Dx;
         if(x > 0 && y > 0 && quad_evitado == 1){
-            printf("%d passos em y e %d passos em x", abs(Dy), abs(Dx));
+            printf("%d passos em y e %d passos em x\n", abs(Dy), abs(Dx));
         }
         else if(x < 0 && y > 0 && quad_evitado == 2){
-            printf("%d passos em y e %d passos em x", abs(Dy), abs(Dx));
+            printf("%d passos em y e %d passos em x\n", abs(Dy), abs(Dx));
         }
         else if(x < 0 && y < 0 && quad_evitado == 3){
-            printf("%d passos em y e %d passos em x", abs(Dy), abs(Dx));
+            printf("%d passos em y e %d passos em x\n", abs(Dy), abs(Dx));
         }
         else if(x > 0 && y < 0 && quad_evitado == 4){
-            printf("%d passos em y e %d passos em x", abs(Dy), abs(Dx));
+            printf("%d passos em y e %d passos em x\n", abs(Dy), abs(Dx));
         }
         else{
-            printf("%d passos em x e %d passos em y", abs(Dx), abs(Dy));
+            printf("%d passos em x e %d passos em y\n", abs(Dx), abs(Dy));
         }
     }
     return 0;
