09f448d576a5faf72422f2a86433a770b949d5fa
miqueias-santos <m.qussantos@gmail.com>
2026-03-11 10:56:54 -0300

    Lista01 finalizada

--- list01/l1_ex02.c
+++ list01/l1_ex02.c
@@ -12,9 +12,9 @@ int main(void){
 
     //as porcentagens retornaram inteiros
     if(value_x == (int)value_x && value_y == (int)value_y && value_z == (int)value_z){
-        printf("Cada homem ficou com %d, %d e %d reais, respectivamente\n", (int)value_x, (int)value_y, (int)value_z);
         int soma = (int)value_x + (int)value_y + (int)value_z;
-                
+        
+        printf("Cada homem ficou com %d, %d e %d reais, respectivamente\n", (int)value_x, (int)value_y, (int)value_z);
         //sobrou, e Rebeka conseguiu dinheiro
         if((total - soma) >= 4) printf("Ela conseguiu! Rebeka voltou para casa e apanhou da mae por sumir noite passada!\n");
         //não sobrou
@@ -24,6 +24,7 @@ int main(void){
 
 
     else{ //não retornam inteiros. Rebeka vai adicionar
+        
         total += 1; //vai investir até resolver o problema e sobrar
         acum = total;
         value_x = (x*total)/100.f;
@@ -32,18 +33,15 @@ int main(void){
         //ela pensa em somar 1 e resolveria o problema de divisão
         
         if(value_x == (int)value_x && value_y == (int)value_y && value_z == (int)value_z){
-            printf("Cada homem ficou com %d, %d e %d reais, respectivamente\n", (int)value_x, (int)value_y, (int)value_z); //print mesmo sem investir
             int soma = (int)value_x + (int)value_y + (int)value_z;
 
             //sobra 2 ou mais pra ela
             if((total - soma) >= 2){ //investe
+                printf("Cada homem ficou com %d, %d e %d reais, respectivamente\n", (int)value_x, (int)value_y, (int)value_z); //print mesmo sem investir
                 
-                if((total - soma) >= 5) {//deu 1, tem 2, precisa de 5
-                    printf("Ela conseguiu! Rebeka voltou para casa e apanhou da mae por sumir noite passada!\n");
-                }
-                else{
-                    printf("E parece que Rebeka vai ter que voltar andando...\n");
-                }
+                //deu 1, tem 2, precisa de 5
+                if((total - soma) >= 5) printf("Ela conseguiu! Rebeka voltou para casa e apanhou da mae por sumir noite passada!\n");
+                else printf("E parece que Rebeka vai ter que voltar andando...\n");
             }
             else total +=1; //não investe 1
             
@@ -57,22 +55,18 @@ int main(void){
             value_z = (z*total)/100.f;
 
             if(value_x == (int)value_x && value_y == (int)value_y && value_z == (int)value_z){
-                printf("Cada homem ficou com %d, %d e %d reais, respectivamente\n", (int)value_x, (int)value_y, (int)value_z);
                 int soma = (int)value_x + (int)value_y + (int)value_z;
 
                 //sobra 3 ou mais pra ela
                 if((total - soma) >= 3){ //investe
-                    
+                    printf("Cada homem ficou com %d, %d e %d reais, respectivamente\n", (int)value_x, (int)value_y, (int)value_z);
                     unsigned char l1, l2, l3;
                     scanf(" %c %c %c", &l1, &l2, &l3);
                     printf("%d\n", l1%96 + l2%96 + l3%96);
                     
-                    if((total - soma) >= 6) {//deu 2, tem 1, precisa de 7, ou seja, no mínimo 6
-                        printf("Ela conseguiu! Rebeka voltou para casa e apanhou da mae por sumir noite passada!\n");
-                    }
-                    else{
-                        printf("E parece que Rebeka vai ter que voltar andando...\n");
-                    }
+                    //deu 2, tem 1, precisa de 7, ou seja, no mínimo 6
+                    if((total - soma) >= 6) printf("Ela conseguiu! Rebeka voltou para casa e apanhou da mae por sumir noite passada!\n");
+                    else printf("E parece que Rebeka vai ter que voltar andando...\n");
                 }
                 else total +=1; //não investe 2
             }
@@ -80,15 +74,15 @@ int main(void){
         }
 
         if(total - acum == 1){
-            //acum = total; não precisa
             value_x = (x*total)/100.f;
             value_y = (y*total)/100.f;
             value_z = (z*total)/100.f;
+            
             if(value_x == (int)value_x && value_y == (int)value_y && value_z == (int)value_z){//divisão se ajeita adicionando 3
-                printf("Cada homem ficou com %d, %d e %d reais, respectivamente\n", (int)value_x, (int)value_y, (int)value_z);
-
                 int soma = (int)value_x + (int)value_y + (int)value_z;
+                
                 if((total - soma) >= 4){ //ela adiciona por vale pra ela
+                    printf("Cada homem ficou com %d, %d e %d reais, respectivamente\n", (int)value_x, (int)value_y, (int)value_z);
                     int id1, id2, id3, s;
                     scanf("%d %d %d", &id1, &id2, &id3);
                     if(id1%3 == 0) s +=(id1/3);
@@ -96,16 +90,13 @@ int main(void){
                     if(id3%3 == 0) s +=(id3/3);
                     printf("%d\n", s);
 
-                    if((total - soma) >= 7){//deu o total
-                        printf("Ela conseguiu! Rebeka voltou para casa e apanhou da mae por sumir noite passada!\n");
-                    }else{ //ainda assim não deu o total
-                        printf("E parece que Rebeka vai ter que voltar andando...\n");
-                    }
+                    //deu o total
+                    if((total - soma) >= 7) printf("Ela conseguiu! Rebeka voltou para casa e apanhou da mae por sumir noite passada!\n");
+                    //ainda assim não deu o total
+                    else printf("E parece que Rebeka vai ter que voltar andando...\n");
 
-                }
-                else{ //ela não adiciona porque não vale
-                    printf("E parece que Rebeka vai ter que voltar andando...\n");
-                }
+                }//ela não adiciona porque não vale
+                else printf("E parece que Rebeka vai ter que voltar andando...\n"); 
             }
             else{ //nem assim a divisão se ajeitou
                 printf("Nao foi dessa vez que Rebeka pode ajudar...\n");
